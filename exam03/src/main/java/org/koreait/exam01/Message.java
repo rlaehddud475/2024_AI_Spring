7eb5e25065b567da6945dbbddb14865fb8a8820b
TwoNewbie <hgwon4071@gmail.com>
2024-11-05 12:43:29 +0900

    20241105

---
+++ exam03/src/main/java/org/koreait/exam01/Message.java
@@ -0,0 +1,14 @@
+package org.koreait.exam01;
+
+import org.springframework.beans.factory.InitializingBean;
+
+public class Message implements InitializingBean {
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("afterPropertiesSet() 호출");
+    }
+
+    public void send(String message){
+        System.out.printf("메세지:%s, 전송완료!%n", message);
+    }
+}
---
+++ exam03/src/test/java/org/koreait/exam01/Ex01.java
@@ -0,0 +1,12 @@
+package org.koreait.exam01;
+
+import org.junit.jupiter.api.Test;
+import org.koreait.exam01.config.AppCtx;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Ex01 {
+    @Test
+    void test1(){
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx.class);
+    }
+}
