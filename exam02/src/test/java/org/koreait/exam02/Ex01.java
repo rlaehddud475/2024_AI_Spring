3e6a5b1b6b8449b5e886820667eade95f1ecf4c4
TwoNewbie <hgwon4071@gmail.com>
2024-11-05 10:02:24 +0900

    20241105

--- exam02/src/test/java/org/koreait/exam02/Ex01.java
+++ exam02/src/test/java/org/koreait/exam02/Ex01.java
@@ -2,6 +2,7 @@ package org.koreait.exam02;
 
 import org.junit.jupiter.api.Test;
 import org.koreait.global.configs.AppCtx4;
+import org.koreait.member.services.InfoService;
 import org.koreait.member.services.JoinService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -11,6 +12,8 @@ public class Ex01 {
     void test1(){
         AnnotationConfigApplicationContext ctx=new AnnotationConfigApplicationContext(AppCtx4.class);
         JoinService joinService = ctx.getBean(JoinService.class);
+        InfoService infoService = ctx.getBean(InfoService.class);
+
         ctx.close();
     }
 }
