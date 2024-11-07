4ed6ccb85a4d3b76be2aa3075de6eba9ffff08fd
TwoNewbie <hgwon4071@gmail.com>
2024-11-07 10:08:11 +0900

    20241107

--- exam04/src/test/java/org/koreait/exam01/Ex02.java
+++ exam04/src/test/java/org/koreait/exam01/Ex02.java
@@ -12,12 +12,20 @@ public class Ex02 {
 
     @Test
     void test1(){
+  /*연결 설정 S*/
     DataSource ds= new DataSource();
     ds.setDriverClassName("oracle.jdbc.driver.OracleDriver");
-    ds.setUrl("jdbc:oracle:thin:@localhost:XE");
+    ds.setUrl("jdbc:oracle:thin:@localhost:1521:XE");
     ds.setUsername("spring6");
     ds.setPassword("oracle");
-
+        /*연결 설정 E*/
+        /*커넥션 풀 설정 S*/
+        ds.setInitialSize(2);
+        ds.setMaxActive(10);
+        ds.setTestWhileIdle(true);
+        ds.setTimeBetweenEvictionRunsMillis(10*1000); //10초 간격으로 유효 커넥션 연결 상태 체크
+        ds.setMinEvictableIdleTimeMillis(50*1000);//50초가 지난 유효 객체 소명 새로운 객체로 생성
+        /*커넥션 풀 설정 E*/
 try( Connection conn = ds.getConnection()) {
     System.out.println(conn);
 
