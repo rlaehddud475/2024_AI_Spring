35ac8983e4c7183a424cdd5b0607ee89fb1779c3
TwoNewbie <hgwon4071@gmail.com>
2024-11-07 09:57:08 +0900

    20241107

--- exam04/src/test/java/org/koreait/exam01/Ex02.java
+++ exam04/src/test/java/org/koreait/exam01/Ex02.java
@@ -2,16 +2,27 @@ package org.koreait.exam01;
 
 import org.apache.tomcat.jdbc.pool.DataSource;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 
 public class Ex02 {
 
-    @BeforeEach
-    void init(){
+    @Test
+    void test1(){
     DataSource ds= new DataSource();
     ds.setDriverClassName("oracle.jdbc.driver.OracleDriver");
     ds.setUrl("jdbc:oracle:thin:@localhost:XE");
     ds.setUsername("spring6");
     ds.setPassword("oracle");
+
+try( Connection conn = ds.getConnection()) {
+    System.out.println(conn);
+
+}catch (SQLException e){
+    e.printStackTrace();
+}
     }
 }
