4420bce23df2c70fe1a286fcefae195f254388a7
TwoNewbie <hgwon4071@gmail.com>
2024-11-07 09:53:25 +0900

    20241109

---
+++ exam04/src/test/java/org/koreait/exam01/Ex02.java
@@ -0,0 +1,17 @@
+package org.koreait.exam01;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.junit.jupiter.api.BeforeEach;
+
+
+public class Ex02 {
+
+    @BeforeEach
+    void init(){
+    DataSource ds= new DataSource();
+    ds.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+    ds.setUrl("jdbc:oracle:thin:@localhost:XE");
+    ds.setUsername("spring6");
+    ds.setPassword("oracle");
+    }
+}
