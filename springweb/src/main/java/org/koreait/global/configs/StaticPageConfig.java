78a2313ac21149f4f8c42ca67d366493a3a1b9dd
TwoNewbie <hgwon4071@gmail.com>
2024-11-11 17:23:17 +0900

    20241111

--- springweb/src/main/java/org/koreait/global/configs/StaticPageConfig.java
+++ springweb/src/main/java/org/koreait/global/configs/StaticPageConfig.java
@@ -1,9 +1,14 @@
 package org.koreait.global.configs;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/*컨트롤러 빈이 없는 정정 페이지 연결 설정*/
 @Configuration
 public class StaticPageConfig implements WebMvcConfigurer {
-
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/myPage").setViewName("member/myPage");
+    }
 }
